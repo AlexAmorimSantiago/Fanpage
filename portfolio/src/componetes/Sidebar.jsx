5c9c23bd02a958b8a8cba1325803fc653c086221
ALEX.A.SANTIAGO <alexsantiago87@yahoo.com.br>
2023-05-23 20:55:14 -0300

    continuação

--- portfolio/src/componetes/Sidebar.jsx
+++ portfolio/src/componetes/Sidebar.jsx
@@ -1,12 +1,16 @@
-import Avatar from '../img/fotoportfolio.jpg'
+import Avatar from "../img/fotoportfolio.jpg"
+
 import"../styles/componetes/sidebar.sass"
+import SocialNetwork from './SocialNetwork'
+
+
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
       <img src={Avatar} alt="Alex Amorim Santiago"></img>
       <p className="title">Desenvolvedor</p>
-      <p>redes sociais</p>
+      <SocialNetwork/>
       <p>informações de contato</p>
       <a href=""className="btn">
         Download curriculo
---
+++ portfolio/src/componetes/SocialNetwork.jsx
@@ -0,0 +1,12 @@
+import {FaLinkedinIn, FaGithub, FaInstagram} from 'react-icons/fa'
+
+import '../styles/componetes/socialnetwork'
+
+
+const SocialNetwork = () => {
+  return (
+    <div>SocialNetwork</div>
+  )
+}
+
+export default SocialNetwork
